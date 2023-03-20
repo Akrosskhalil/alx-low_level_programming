d9e8082c3182bb3cb6fe2f2928c9190748e4bad2
Akrosskhalil <khalilrossayssi@gmail.com>
2023-03-20 12:20:10 -0700

    print function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
+/**
+ * main - a program with printf function
+ * Return: 0 (success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n")
-	retun (0);
+	return (0);
 }
